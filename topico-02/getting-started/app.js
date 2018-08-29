538b62e98586e18a9639f23c4015a250ec925301
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2018-08-29 18:39:32 -0300

    mensagens removidas (apenas o necessario deve ser mantido)

--- topico-02/getting-started/app.js
+++ topico-02/getting-started/app.js
@@ -1,19 +1,14 @@
-//importação dos modulos do protocolo http na constante http
 const http = require('http');
-// definição do host em uma constante
 const hostname = '127.0.0.1';
-// definição da porta
 const port = 3000;
-//criação do servidor http
+
+//criação do servidor http (única resposta para qualquer requisição)
 const server = http.createServer((req, res) => {
-    // resposta assincroa ao cliente de status OK
   res.statusCode = 200;
-  //resposta no Header definindo o tipo da mensagem na pagina html
   res.setHeader('Content-Type','text/plain' );
-  //resposta e ultima mensagem enviada
   res.end('Trabalhando com Nodejs');
 });
-//escreve no servidor a porta e o host conectado
+
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
