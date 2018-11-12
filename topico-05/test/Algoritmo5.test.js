13afd02f564f5f9afe6cddd71e5367cd267ae9fc
Antonio Tavares <antoniotavarespereira@gmail.com>
2018-11-12 10:44:09 -0200

    adicionou algoritmo5 e seu teste

---
+++ topico-05/test/Algoritmo5.test.js
@@ -0,0 +1,16 @@
+var somaNaturais = require("../Algoritmo5");
+
+test("Somatorio de todos os valores de 2", ()=>{
+    expect(somaNaturais.somaNaturais(2)).toBe(3);
+});
+
+
+test("Somatorio de todos os valores de 3", ()=>{
+    expect(somaNaturais.somaNaturais(3)).toBe(6);
+});
+test("Somatorio de todos os valores de 2", ()=>{
+    expect(somaNaturais.somaNaturais(4)).toBe(10);
+});
+test("Somatorio de todos os valores de 2", ()=>{
+    expect(somaNaturais.somaNaturais(5)).toBe(15);
+});
