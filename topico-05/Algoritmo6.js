b2ef6d29e10083a90e3f7014191b0e69af89beab
Antonio Tavares <antoniotavarespereira@gmail.com>
2018-11-12 12:21:39 -0200

    adicionou algoritmo 6

---
+++ topico-05/Algoritmo6.js
@@ -0,0 +1,21 @@
+/**Esta função faz o calculo do fatorial de um numero natural, para isso
+ * ela recebe um numero natural mair que 1(um)e retorna o seu fatorial ]
+ * @param {int} numero Natural
+ * @returns{int} fatorial do numero natural
+ * */
+
+ var fatorial = function(numeroNatural){
+    if(numeroNatural<=1){
+         return false;
+     }else{
+       var indice = 2;
+       var fatorial = 1;
+       while(indice <= numeroNatural){
+           fatorial = fatorial * indice;
+           indice++;
+        }
+        return fatorial
+    }
+}
+ 
+ module.exports.fatorial = fatorial;
