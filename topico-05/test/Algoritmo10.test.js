d65ccb13bafd2689632df35ae9f87ad6e52643d2
Antonio Tavares <antoniotavarespereira@gmail.com>
2018-11-13 00:23:22 -0200

    não estou conseguindo analisar o problema direito, otmizarei na proxima versao

---
+++ topico-05/test/Algoritmo10.test.js
@@ -0,0 +1,21 @@
+
+
+var logaritimoNatural = require("../Algoritmo10.js");
+
+
+test("Valor da potencia invialido", ()=>{
+    expect(0).toBe(0);
+});
+
+test("potencia 10",()=>{
+    expect(10).toBe(10);
+})
+test("potencia 2",()=>{
+    expect(2).toBe(2);
+})
+test("potencia 3",()=>{
+    expect(3).toBe(3);
+})
+test("potencia 4",()=>{
+    expect(4).toBe(4);
+})
