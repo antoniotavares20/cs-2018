6a8bbdf327af1cec0bc7f52f9f8b220702bc5330
Antonio Tavares <antoniotavarespereira@gmail.com>
2018-11-13 23:59:36 -0200

    aplicando refatoração

--- topico-03/prompt.js
+++ topico-03/prompt.js
@@ -1,6 +1,15 @@
-
-let i;
-
-while(i<20){
-    prompt(" PARA FLEXOES "+ i+1);
-}
+{
+    const w = Math.min(640, width);
+    const h = 320;
+    const r = 20;
+    const t = 1500;
+    const svg = d3.select(DOM.svg(w, h));
+    const circle = svg.append("circle").attr("r", r).attr("cx", w / 4).attr("cy", h / 4);
+    while (true) {
+      yield svg.node();
+      await circle.transition().duration(t).attr("cy", h * 3 / 4).end();
+      await circle.transition().duration(t).attr("cx", w * 3 / 4).end();
+      await circle.transition().duration(t).attr("cy", h * 1 / 4).end();
+      await circle.transition().duration(t).attr("cx", w * 1 / 4).end();
+    }
+  }
