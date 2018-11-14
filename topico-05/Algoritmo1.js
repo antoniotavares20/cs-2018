87490dc96d196f3882c6477bcae669fe21a8f474
Antonio Tavares <antoniotavarespereira@gmail.com>
2018-11-14 00:05:49 -0200

    aplicando refatoração

--- topico-05/Algoritmo1.js
+++ topico-05/Algoritmo1.js
@@ -1,11 +1,13 @@
-//propiedade 3025
+/**Esta função cumpre a propriedade 3025 
+ * @param {int} para os dois primeiros numeros(milhar e centena) ;
+ * @param {int} dois ultimos numeros (numeral e decimal)
+ * @param {float} valida se o numero faz parte ou não desta propriedade;
+ *
+ */
 
 function propriedade3025(abcd) {
-    // se nao satisfaz exigencia gera excecao
-
     let ab = doisPrimeirosDigitos(abcd);
     let cd = doisUltimosDigitos(abcd);
-
     return (ab + cd) * (ab + cd) == abcd;
 }
 
