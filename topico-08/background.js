e76aeefd9ea39320595897cb458e55ca29a224cc
aluno <antoniotavares20>
2018-11-21 21:58:09 -0200

    adicionou manifesto e configurações

---
+++ topico-08/background.js
@@ -0,0 +1,12 @@
+/**Eventos importantes de segundo plano da extenção cronometro
+ * 
+ */
+
+
+'use strict';
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.sync.set({color: '#3aa757'}, function() {
+    console.log("The color is green.");
+  });
+});
