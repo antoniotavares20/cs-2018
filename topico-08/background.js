105a89a543d67cd5264f8ca829ddddae9eb06f12
aluno <antoniotavares20>
2018-11-28 21:54:22 -0200

    adicionou novas atualizacoes2

--- topico-08/background.js
+++ topico-08/background.js
@@ -7,9 +7,9 @@ chrome.runtime.onInstalled.addListener(function() {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {schemes: ['https']},
+        pageUrl: {schemes: ['https','http']},
       })],
       actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
   });
-});
+});1
