294ea7b51c2f92b774e6e0349e060f240ff3f99a
Antonio Tavares <antoniotavarespereira@gmail.com>
2018-12-04 23:58:43 -0200

    adicionando logo e teste

--- topico-08/test/cronometro.test.js
+++ topico-08/test/cronometro.test.js
@@ -1,6 +1,41 @@
-var cronometro = require("../cronometro.js");
+var cronometro = require("../model.js");
 
 /* Teste do temporizador de segundo*/
 test("retorno do tempo", () => {
-    expect(cronometro.ini("iniciar")).toBe(true);
+    expect(cronometro.limparTempo(0)).toBe(true);
 })
+
+/**teste da função regras do tempo
+ * é esperado que a entrada de três atributos
+ * hora minuto e segundo retornem valores maior do quê um 
+ * 
+ * /
+ 
+/** As horas não podem ser maior do que 24
+ * 
+*/
+test("verificando se notas sejam maior do que 24" ()={
+//  expect().toBe();
+})
+
+
+/** Os minutos não podem  ser maior do que 60*/
+
+
+/** Os segundos não podem ser maior do que 60*/
+
+//função decremento
+/** Os valores de entrada para contagem regressiva, deve sempre ser maior que 1 */
+
+/**quando o segundo for zero, necessáriamente o demcremento não deve ocorrer */
+
+/**A função após ser executada o "horario" deve ser falsa quando for maior do que zero */
+
+
+
+/**A função após ser executada o "minuto" deve ser falsa quando for maior do que zero */
+
+
+/**A função após ser executada o "segundo" deve ser falsa quando for maior do que zero */
+
+
