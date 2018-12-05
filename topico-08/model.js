ae4d70e8230cb1efbb87b50cb8cd1e7fd9e25858
Antonio Tavares <antoniotavarespereira@gmail.com>
2018-12-04 22:31:57 -0200

    adicionou comentarios

--- topico-08/model.js
+++ topico-08/model.js
@@ -4,7 +4,14 @@ var hora = 0;
 var minuto = 0;
 var segundo = FREQUENCIA;
 var intervalo = { hora, minuto, segundo };
+var tempo;
 
+/**Esta função recebe os intervalos de 
+ * data hora e tempo para validar a progressão 
+ * temporal de acordo com os padrões existentes
+ * para a hora, minuto e segundo
+ * @param {object} intervalo = {hora, minuto, segundo} 
+ */
 function regrasTempo(intervalo) {
   if (intervalo.segundo == 60) {
     intervalo.minuto++;
@@ -18,10 +25,14 @@ function regrasTempo(intervalo) {
   intervalo.segundo++;
   return intervalo;
 }
-
+/** Esta função trata as regras para contagem regressiva
+ * do tempo fazendo o temporizador retroceder;
+ * @param {void}
+ **/
 function regrasContagemRegressiva() {
-	intervalo.hora = 1;
-	intervalo.minuto = 15;
+	intervalo.hora = 0;
+  intervalo.minuto = 1;
+
   if (intervalo.minuto == 60) {
     intervalo.hora--;
     intervalo.segundo = 0;
@@ -34,7 +45,10 @@ function regrasContagemRegressiva() {
   intervalo.segundo--;
   return intervalo;
 }
-
+/**Esta função zera os valores contidos no 
+ * itervalo
+ * @param {object} intervalo = {hora, minuto, segundo} 
+ */
 function limparTempo(intervalo) {
   intervalo.segundo = 0;
   intervalo.hora = 0;
@@ -42,6 +56,12 @@ function limparTempo(intervalo) {
 }
 
 /**View */
+
+/** Esta função faz a manipulação de objetos
+ * nas tags ID para atualizar o tempo no DOM
+ * atualizando o tempo no "cronometro.html" 
+ * @param {*} intervalo 
+ */
 function escreverTempo(intervalo) {
   if (intervalo.segundo < 10) {
     console.log(intervalo.segundo);
@@ -56,43 +76,52 @@ function escreverTempo(intervalo) {
 }
 
 /**Controller Suport*/
-
+/**Esta de acordo com as regras do tempo e
+ * envia o tempo atual para a função escrever
+ * tempo. 
+ */
 function atualizarTempo() {
   intervalo = regrasTempo(intervalo);
   escreverTempo(intervalo);
 }
-
+/**Esta função inicializa a contagem regressiva para 
+ * retroceder o tempo e envia parar o tempo marcado 
+ * para a função escrever tempo;
+ */
 function contagemRegressiva() {
   intervalo = regrasContagemRegressiva(intervalo);
   escreverTempo(intervalo);
 }
-
-var tempo;
+/**Esta função iniciar o camando por meio do protocolo
+ * setInterval, a contagem dos segundos
+ */
 function iniciar() {
   console.log("iniciar");
   parar();
   tempo = setInterval(atualizarTempo, 1000);
 }
-
+/**Esta função realiza a parada do protocolo interval;*/
 function parar() {
   console.log("parar");
   clearInterval(this.tempo);
 }
-
+/**Esta função inverte a contagem do tempo*/
 function retroceder() {
   console.log("iniciar contagem regressiva");
   parar(); 
   limpar();	
   this.tempo = setInterval(contagemRegressiva, 1000);
 }
-
+/**esta função limpa os valores do cronmetro atual */
 function limpar() {
   limparTempo(this.intervalo);
   escreverTempo(this.intervalo);
 }
 
 /**Controler-io */
-
+/**As função abaixo, escultam os eventos da DOM e
+ * execultam as funções de controle as elas vinculadas
+ */
 window.addEventListener("load", function load(event) {
   var createButton = document.getElementById(["iniciar"]);
   createButton.addEventListener("click", function() {
